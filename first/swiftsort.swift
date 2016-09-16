d84fcbc5c2442a58b12824f2a5047613e917623e
Alexander <shiale14@student.westerdals.no>
2016-09-16 23:44:09 +0200

    add solution for exercise 11

--- first/swiftsort.swift
+++ first/swiftsort.swift
@@ -1,1 +1,11 @@
-#!/usr/bin/env xcrun swift import Cocoa print(“Hello world”)
+#!/usr/bin/env xcrun swift 
+
+import Cocoa
+import Foundation
+
+if let text = readLine() {
+    let array = text.components(separatedBy: " ")
+    print(array.sorted { $0.localizedCaseInsensitiveCompare($1) == ComparisonResult.orderedAscending })
+} else {
+    print("No input.")
+}
