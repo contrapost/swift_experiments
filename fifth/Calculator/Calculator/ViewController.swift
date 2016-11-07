fc29b711708d512d7dfca00572d17e6f98f5920a
Alexander <shiale14@student.westerdals.no>
2016-11-07 22:46:43 +0100

    add first stanford lecture exercise

---
+++ fifth/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Alexander Shipunov on 07/11/2016.
+//  Copyright © 2016 Alexander Shipunov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+   
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+    }
+
+}
+
